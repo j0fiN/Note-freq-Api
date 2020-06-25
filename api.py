44c4a801474263809e6f93f9eadb906141d2ebc1
j0fiN <jofinfab@gmail.com>
2020-06-25 11:37:28 +0530

    DEV:create a simple server

--- api.py
+++ api.py
@@ -1,2 +1,19 @@
 from flask import *
+from frequency import Note_Freq
+from wsgiref.simple_server import make_server
+api = Flask(__name__)
 
+@api.route('/api/<string:s>', methods=["GET"])
+def getter(s):
+    note = Note_Freq(s)
+    if note.frequency() is False:
+        return abort(400)
+    else:
+        return jsonify(dict(
+            note=note.N,
+            octave=note.octave,
+            frequency=note.frequency()
+        )), 200
+
+if __name__ =="__main__":
+    api.run()
