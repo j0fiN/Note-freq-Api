5a55e150a8e1ecd890f662326d951f5e692d2918
j0fiN <jofinfab@gmail.com>
2020-06-25 09:12:14 +0530

    TEST: for frequency check :sparkle:

--- tests/test_algo.py
+++ tests/test_algo.py
@@ -1,10 +1,11 @@
 import unittest
 from frequency import Note_Freq
 
+
 class MyTestCase(unittest.TestCase):
     def test_0(self):
         n = Note_Freq("a4")
-        self.assertAlmostEquals(n.frequency(), 440)
+        self.assertAlmostEqual(n.frequency(), 440)
         self.assertEquals(str(type(n.frequency())), "<class 'float'>")
 
     def test_1(self):
@@ -13,7 +14,7 @@ class MyTestCase(unittest.TestCase):
 
     def test_2(self):
         n = Note_Freq("A4")
-        self.assertAlmostEquals(n.frequency(), 440)
+        self.assertAlmostEqual(n.frequency(), 440)
 
     def test_3(self):
         n = Note_Freq("4a")
@@ -30,6 +31,16 @@ class MyTestCase(unittest.TestCase):
     def test_6(self):
         n = Note_Freq("@$%^")
         self.assertEquals(False, n.frequency())
+
+    def test_7(self):
+        n = Note_Freq("G5")
+        self.assertAlmostEqual(1568.00, n.frequency(), 1)
+
+    def test_8(self):
+        n = Note_Freq("G#1")
+        self.assertAlmostEqual(103.83, n.frequency(), 1)
+
+
 
 
 if __name__ == '__main__':
